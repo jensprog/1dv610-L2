18d0e4ffaed583eea4cae13b8ffef81493ee0d1a
Jens <jk224vx@student.lnu.se>
2025-09-13 16:33:20 +0200

    Added another test for temp converter.

--- src/test/java/com/jensprog/unitconverter/TemperatureConverterTest.java
+++ src/test/java/com/jensprog/unitconverter/TemperatureConverterTest.java
@@ -12,4 +12,11 @@ class TemperatureConverterTest {
     double result = temperatureConverter.convert(0);
     assertEquals(32, result, 0.01);
   }
+
+  @Test
+  void testFahrenheitToCelsius() {
+    TemperatureConverter temperatureConverter = new TemperatureConverter("Fahrenheit", "Celsius");
+    double result = temperatureConverter.convert(32);
+    assertEquals(0, result, 0.01);
+  }
 }
