25de321b070cde32e6fdeff271730d940d58d63e
Jens <jk224vx@student.lnu.se>
2025-09-13 20:12:17 +0200

    Added more tests to length conversion.

--- src/main/java/com/jensprog/App.java
+++ src/main/java/com/jensprog/App.java
@@ -5,12 +5,12 @@ import com.jensprog.unitconverter.TemperatureConverter;
 
 public class App {
   public static void main(String[] args) {
-    TemperatureConverter temperatureConverter = new TemperatureConverter("Celsius", "Fahrenheit");
-    double fahrenheit = temperatureConverter.convert(-1);
-    System.out.println("-1 Celsius is " + fahrenheit + " Fahrenheit");
+    // TemperatureConverter temperatureConverter = new TemperatureConverter("Celsius", "Fahrenheit");
+    // double fahrenheit = temperatureConverter.convert(-1);
+    // System.out.println("-1 Celsius is " + fahrenheit + " Fahrenheit");
 
-    LengthConverter lengthConverter = new LengthConverter("inch", "kilometer");
-    double kilometers = lengthConverter.convert(1);
-    System.out.println("1 inch is " + kilometers + " kilometers");
+    LengthConverter lengthConverter = new LengthConverter("meter", "centimeter");
+    double centimeters = lengthConverter.convert(1);
+    System.out.println("1 meter is " + centimeters + " centimeters");
   }
 }
--- src/test/java/com/jensprog/unitconverter/LengthConverterTest.java
+++ src/test/java/com/jensprog/unitconverter/LengthConverterTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 class LengthConverterTest {
   @Test
-  void testInchToKilometer() {
+  public void testInchToKilometer() {
     LengthConverter lengthConverter = new LengthConverter("inch", "kilometer");
     double input = 1;
     double expected = 0.0000254;
@@ -19,5 +19,22 @@ class LengthConverterTest {
     input = 10000000;
     expected = 254;
     assertEquals(expected, lengthConverter.convert(input), 0.0001);
+  }
+
+  @Test
+  public void testCentimeterToMillimeter() {
+    LengthConverter lengthConverter = new LengthConverter("centimeter", "millimeter");
+    double input = 10;
+    double expected = 100;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+    input = 10000;
+    expected = 100000;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+    input = 0;
+    expected = 0;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+    
   }
 }
