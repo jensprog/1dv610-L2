a800d1cb47117508d1c532c5c981c9de482449fc
Jens <jk224vx@student.lnu.se>
2025-09-14 16:44:10 +0200

    Added a few more automated tests for length conversion.

--- src/test/java/com/jensprog/unitconverter/LengthConverterTest.java
+++ src/test/java/com/jensprog/unitconverter/LengthConverterTest.java
@@ -6,6 +6,38 @@ import org.junit.jupiter.api.Test;
 
 class LengthConverterTest {
   @Test
+  public void testMeterToMillimeter() {
+    LengthConverter lengthConverter = new LengthConverter("meter", "millimeter");
+    double input = 1;
+    double expected = 1000;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+    input = 0.001;
+    expected = 1;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+    input = 1000;
+    expected = 1000000;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+  }
+
+  @Test
+  public void testMillimeterToMeter() {
+    LengthConverter lengthConverter = new LengthConverter("millimeter", "meter");
+    double input = 1000;
+    double expected = 1;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+    input = 1;
+    expected = 0.001;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+    input = 1000000;
+    expected = 1000;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+  }
+
+  @Test
   public void testInchToKilometer() {
     LengthConverter lengthConverter = new LengthConverter("inch", "kilometer");
     double input = 1;
@@ -18,6 +50,22 @@ class LengthConverterTest {
 
     input = 10000000;
     expected = 254;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+  }
+
+  @Test
+  public void testKilometerToInch() {
+    LengthConverter lengthConverter = new LengthConverter("kilometer", "inch");
+    double input = 1;
+    double expected = 39370.0787;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+    input = 0.0254;
+    expected = 1000;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+    input = 254;
+    expected = 10000000;
     assertEquals(expected, lengthConverter.convert(input), 0.0001);
   }
 
@@ -36,5 +84,54 @@ class LengthConverterTest {
     expected = 0;
     assertEquals(expected, lengthConverter.convert(input), 0.0001);
     
+  }
+
+  @Test
+  public void testMillimeterToCentimeter() {
+    LengthConverter lengthConverter = new LengthConverter("millimeter", "centimeter");
+    double input = 100;
+    double expected = 10;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+    input = 100000;
+    expected = 10000;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+    input = 0;
+    expected = 0;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+  }
+
+  @Test
+  public void testMeterToCentimeter() {
+    LengthConverter lengthConverter = new LengthConverter("meter", "centimeter");
+    double input = 1;
+    double expected = 100;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+    input = 0.01;
+    expected = 1;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+    input = 1000;
+    expected = 100000;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+  }
+
+  @Test
+  public void testCentimeterToMeter() {
+    LengthConverter lengthConverter = new LengthConverter("centimeter", "meter");
+    double input = 100;
+    double expected = 1;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+    input = 1;
+    expected = 0.01;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
+
+    input = 100000;
+    expected = 1000;
+    assertEquals(expected, lengthConverter.convert(input), 0.0001);
   }
 }
