ece702d389f85e8a77c438074c6854be714c0276
Jens <jk224vx@student.lnu.se>
2025-09-14 10:39:41 +0200

    Added tests for temp conversion to cover both directions.

--- src/test/java/com/jensprog/unitconverter/TemperatureConverterTest.java
+++ src/test/java/com/jensprog/unitconverter/TemperatureConverterTest.java
@@ -36,4 +36,68 @@ class TemperatureConverterTest {
     expected = 100;
     assertEquals(expected, temperatureConverter.convert(input), 0.0001);
   }
+
+  @Test
+  public void testCelsiusToKelvin() {
+    TemperatureConverter temperatureConverter = new TemperatureConverter("Celsius", "Kelvin");
+    double input = 0;
+    double expected = 273.15;
+    assertEquals(expected, temperatureConverter.convert(input), 0.0001);
+
+    input = -273.15;
+    expected = 0;
+    assertEquals(expected, temperatureConverter.convert(input), 0.0001);
+
+    input = 100;
+    expected = 373.15;
+    assertEquals(expected, temperatureConverter.convert(input), 0.0001);
+  }
+
+  @Test
+  public void testKelvinToCelsius() {
+    TemperatureConverter temperatureConverter = new TemperatureConverter("Kelvin", "Celsius");
+    double input = 0;
+    double expected = -273.15;
+    assertEquals(expected, temperatureConverter.convert(input), 0.0001);
+
+    input = 273.15;
+    expected = 0;
+    assertEquals(expected, temperatureConverter.convert(input), 0.0001);
+
+    input = 373.15;
+    expected = 100;
+    assertEquals(expected, temperatureConverter.convert(input), 0.0001);
+  }
+
+  @Test
+  public void testFahrenheitToKelvin() {
+    TemperatureConverter temperatureConverter = new TemperatureConverter("Fahrenheit", "Kelvin");
+    double input = 32;
+    double expected = 273.15;
+    assertEquals(expected, temperatureConverter.convert(input), 0.0001);
+
+    input = -459.67;
+    expected = 0;
+    assertEquals(expected, temperatureConverter.convert(input), 0.0001);
+
+    input = 212;
+    expected = 373.15;
+    assertEquals(expected, temperatureConverter.convert(input), 0.0001);
+  }
+
+  @Test
+  public void testKelvinToFahrenheit() {
+    TemperatureConverter temperatureConverter = new TemperatureConverter("Kelvin", "Fahrenheit");
+    double input = 273.15;
+    double expected = 32;
+    assertEquals(expected, temperatureConverter.convert(input), 0.0001);
+
+    input = 0;
+    expected = -459.67;
+    assertEquals(expected, temperatureConverter.convert(input), 0.0001);
+
+    input = 373.15;
+    expected = 212;
+    assertEquals(expected, temperatureConverter.convert(input), 0.0001);
+  }
 }
